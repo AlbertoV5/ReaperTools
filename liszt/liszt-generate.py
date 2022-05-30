9ff25f3aa887e8840d8bd9691e9ed090f4ac5636
AlbertoV5 <avq5ac1@gmail.com>
2022-05-29 20:46:07 -0500

    moving src to repo

---
+++ liszt/liszt-generate.py
@@ -0,0 +1,64 @@
+# @noindex
+
+import tosclib as tosc
+from pathlib import Path
+import xml.etree.ElementTree as ET
+
+
+class FX():
+    def __init__(self, inputPath):
+        with open(inputPath, "r") as file:
+            self.name = ET.fromstring(file.read())
+        self.params = self.name.find("params")
+
+def oscMsg() -> tosc.OSC:
+    """Create a message with a path constructed with custom Partials"""
+    return tosc.OSC(
+        path=[
+            tosc.Partial(),  # Default is the constant '/'
+            tosc.Partial(type="PROPERTY", value="parent.name"),
+            tosc.Partial(),
+            tosc.Partial(type="PROPERTY", value="name"),
+        ]
+    )
+
+def createFader(e: tosc.ElementTOSC, name, width, limit, i, msg):
+    fader = tosc.ElementTOSC(e.createChild("FADER"))
+    fader.createProperty("s", "name", name)
+    fader.setFrame(width * i, 0, width, 1080)
+    fader.setColor(i / limit, 0, 1 - i / limit, 1)
+    fader.createOSC(msg)  # Creates a new message from custom tosc.OSC
+
+
+def main(inputFile, outputFile):
+
+    fx = FX(inputFile)
+
+    root = tosc.createTemplate()
+    base = tosc.ElementTOSC(root[0])
+    base.createProperty("s", "name", "template")
+    base.setFrame(0, 0, 1920, 1080)
+
+    # Group container for the faders
+    group = tosc.ElementTOSC(base.createChild("GROUP"))
+    group.createProperty("s", "name", fx.name.text)
+    group.setFrame(420, 0, 1080, 1080)
+    group.setColor(0.25, 0.25, 0.25, 1)
+
+    # Create faders based on Json data
+    limit = 10
+    width = int(group.getPropertyParam("frame", "w").text) / limit
+    msg = oscMsg()
+
+    for param in fx.params:
+        index = int(param.attrib["index"])
+        createFader(group, param.text, width, limit, index, msg)
+        if index == limit:
+            break
+
+    tosc.write(root, outputFile)
+
+
+if __name__ == "__main__":
+    file = Path(RPR_GetExtState("AlbertoV5-ReaperTools", "liszt_path_1"))
+    main(file, file.parent / f"{str(file.stem)}.tosc")
---
+++ liszt/liszt-pull.py
@@ -0,0 +1,84 @@
+# @noindex
+
+import xml.etree.ElementTree as ET
+
+class FX():
+    def __init__(self, name):
+        self.name = ET.Element("name")
+        self.name.text = name
+        self.params = ET.SubElement(self.name, "params")
+        
+    def append(self, name, index):
+        e = ET.SubElement(self.params, "param", attrib={"index":str(index)})
+        e.text = name
+
+class Converter:
+    def __init__(self):
+        self.mb_title = "Liszt"
+        self.track = None
+        self.fxNum = None
+        self.fxName = None
+        self.FX = None
+        (self.output_path, _) = RPR_GetProjectPath("", 8192)
+        os = RPR_GetOS()
+        self.sep = "\\" if os == "Win32" or os == "Win64" else "/"
+        self.status = True
+
+    def update(self):
+        self.result(self.getLastTouchedFX)
+
+    def save(self):
+        self.result(self.getFXParamsTable)
+        self.result(self.writeFile)
+
+    def getLastTouchedFX(self) -> bool:
+        (_, _, fxnumber, _) = RPR_GetLastTouchedFX(0, 0, 0)
+        self.track = RPR_GetSelectedTrack(0, 0)
+        (retval, _, _, fxName, _) = RPR_TrackFX_GetFXName(
+            self.track, fxnumber, "", 2048
+        )
+        self.fxNum = fxnumber
+        self.fxName = fxName
+        return retval
+
+    def getFXParamsTable(self) -> bool:
+        if not self.fxName:
+            return False
+
+        self.FX = FX(self.fxName)
+        numParams = (
+            RPR_TrackFX_GetNumParams(self.track, self.fxNum) - 1
+        )
+        for i in range(numParams):
+            (_, _, _, _, param, _) = RPR_TrackFX_GetParamName(
+                self.track, self.fxNum, i, "", 2048
+            )
+            if not _:
+                return False
+            if param != "MIDI CC":
+                self.FX.append(param, i)
+
+        return True
+
+    def writeFile(self) -> bool:
+        file_name = self.fxName.split(": ")[1].split(" (")[0]
+        output_file = f"{self.output_path}{self.sep}{file_name}.xml"
+        
+        with open(output_file, "wb") as file:
+            file.write(ET.tostring(self.FX.name, encoding = "UTF-8", xml_declaration=True))
+
+        RPR_SetExtState(
+            "AlbertoV5-ReaperTools", "liszt_path_1", output_file, True
+        )
+        return True
+
+    def result(self, fun, msg: str = ""):
+        if self.status and not fun():
+            msg = f"Failed to {fun.__name__}" if not msg else msg
+            self.status = False
+            return RPR_MB(msg, self.mb_title, 0)
+
+
+converter = Converter()
+converter.update()
+converter.save()
